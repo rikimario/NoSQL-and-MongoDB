f19b363fe709d70ecab8998067a6a54d92e7d3e5
rikimario <mario_martinov@abv.bg>
2023-09-28 19:34:15 +0300

    connecting to db

---
+++ init-mongo-demo/index.js
@@ -0,0 +1,14 @@
+const mongoDb = require('mongodb');
+
+const connectionString = 'mongodb://127.0.0.1:27017';
+const client = new mongoDb.MongoClient(connectionString);
+
+async function connectDb() {
+  client.connect();
+
+  const db = client.db('DogsDB');
+  const dogs = db.collection('dogs');
+  const result = await dogs.find().toArray();
+  console.log(result);
+}
+connectDb();
