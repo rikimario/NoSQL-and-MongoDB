2c5089f39ebea789dc437e217593425a314d8dc7
rikimario <mario_martinov@abv.bg>
2023-09-29 23:50:27 +0300

    require models dog

--- mongoose-demo/index.js
+++ mongoose-demo/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const Dog = require('./models/Dog');
+
+
 const CONNECTION_STR = 'mongodb://127.0.0.1:27017/DogsDB';
 
 async function connectDb() {
   mongoose.connect(CONNECTION_STR);
+
+  const dogs = await Dog.find();
+  console.log(dogs);
 }
 connectDb();
